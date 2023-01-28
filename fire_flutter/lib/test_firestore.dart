6fce3a9dc44131098ff64d603622459f9297a68e
ryn-crypto <riyanfirst13@gmail.com>
2023-01-28 15:52:43 +0700

    menggunakan firestore database untuk crud

--- fire_flutter/lib/test_firestore.dart
+++ fire_flutter/lib/test_firestore.dart
@@ -15,11 +15,46 @@ class _TestFirestoreState extends State<TestFirestore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Test Firebase')),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: db.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error'),
+            );
+          }
+          // olah data
+          var _data = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                onLongPress: () {
+                  // auto delete when long press
+                  _data[index].reference.delete().then((value) =>
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Data Removed'))));
+                },
+                subtitle: Text(_data[index].data()['born'].toString()),
+                title: Text(_data[index].data()['first'] +
+                    ' ' +
+                    _data[index].data()['last']),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           final user = <String, dynamic>{
-            "first": "Ada",
-            "last": "Lovelace",
+            "first": "joko",
+            "last": "Anwar",
             "born": 1815
           };
 
